66672b5ceab3d7dbf88414c8abc9df511fdae95e
jaume-pm <jaumepetit2002@gmail.com>
2024-07-02 10:25:25 +0200

    Initial commit

---
+++ src/main/java/com/Jaume/Koerber/KoerberApplication.java
@@ -0,0 +1,13 @@
+package com.Jaume.Koerber;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KoerberApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KoerberApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/Jaume/Koerber/KoerberApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Jaume.Koerber;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KoerberApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
